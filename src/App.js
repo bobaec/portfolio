1f282f8eef908137028e3030eb53bc1a0c379cc7
bobae <bobae.choi2@gmail.com>
2024-01-09 14:55:43 -0800

    update portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import InfoCard from './components/InfoCard/InfoCard';
-import Header from './components/Header/Header';
-import './App.scss';
-import { personalDescription, cloudArmyInfo, valostatsInfo, buzzItInfo, sfuInfo, stonksInfo, sfuSSEPInfo, mutationAnalysisInfo, studywitmeInfo } from './assets/texts/Texts.js';
+import InfoCard from "./components/InfoCard/InfoCard";
+import Header from "./components/Header/Header";
+import "./App.scss";
+import {
+    personalDescription,
+    cloudArmyInfo,
+    valostatsInfo,
+    buzzItInfo,
+    sfuInfo,
+    stonksInfo,
+    sfuSSEPInfo,
+    mutationAnalysisInfo,
+    studywitmeInfo,
+    portfolioInfo,
+} from "./assets/texts/Texts.js";
 
 function App() {
-  return (
-    <div className="portfolio-container">
-      <Header title="BOBAE CHOI" description={personalDescription} socials={true} />
-      <div className="info-cards-container">
-        {/* <InfoCard info={glanceInfo} /> */}
-        <InfoCard info={cloudArmyInfo} />
-        <InfoCard info={valostatsInfo} />
-        <InfoCard info={buzzItInfo}/>
-        <InfoCard info={sfuInfo} />
-        <InfoCard info={stonksInfo} />
-        <InfoCard info={mutationAnalysisInfo} />
-        <InfoCard info={sfuSSEPInfo} />
-        <InfoCard info={studywitmeInfo} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="portfolio-container">
+            <Header
+                title="BOBAE CHOI"
+                description={personalDescription}
+                socials={true}
+            />
+            <div className="info-cards-container">
+                {/* <InfoCard info={glanceInfo} /> */}
+                <InfoCard info={cloudArmyInfo} />
+                <InfoCard info={portfolioInfo} />
+                <InfoCard info={valostatsInfo} />
+                <InfoCard info={buzzItInfo} />
+                <InfoCard info={sfuInfo} />
+                <InfoCard info={stonksInfo} />
+                <InfoCard info={mutationAnalysisInfo} />
+                <InfoCard info={sfuSSEPInfo} />
+                <InfoCard info={studywitmeInfo} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/assets/texts/Texts.js
+++ src/assets/texts/Texts.js
@@ -1,123 +1,173 @@
-import cloudArmyLogo from '../../assets/logos/cloudarmy-logo.png';
-import valostatsLogo from '../../assets/logos/valostats-logo.png';
-import buzzItLogo from '../../assets/logos/buzzit-logo.png';
-import sfuLogo from '../../assets/logos/sfu-logo.png';
-import mutationAnalysisLogo from '../../assets/logos/mutation-testing-logo.png';
-import studywitmeLogo from '../../assets/logos/studywitme-logo.png';
-import stonksLogo from '../../assets/logos/stonks-logo.png';
-import glanceLogo from '../../assets/logos/glance-logo.png';
+import cloudArmyLogo from "../../assets/logos/cloudarmy-logo.png";
+import valostatsLogo from "../../assets/logos/valostats-logo.png";
+import buzzItLogo from "../../assets/logos/buzzit-logo.png";
+import sfuLogo from "../../assets/logos/sfu-logo.png";
+import mutationAnalysisLogo from "../../assets/logos/mutation-testing-logo.png";
+import studywitmeLogo from "../../assets/logos/studywitme-logo.png";
+import stonksLogo from "../../assets/logos/stonks-logo.png";
+import glanceLogo from "../../assets/logos/glance-logo.png";
+import portfolioLogo from "../../assets/logos/portfolio-logo.png";
 
-// const personalDescriptionOld = 
+// const personalDescriptionOld =
 // 'I am a Front End Developer with a three year track record in creating engaging and user-friendly web interfaces. With a typing speed of 150 words per minute, I am efficient at turning my ideas into code.';
-const personalDescription = '';
+const personalDescription = "";
 
 const cloudArmyInfo = {
     image: cloudArmyLogo,
-    title: 'CloudArmy',
-    duration: 'July 2021 - Oct 2023',
-    jobTitle: 'Software Developer',
-    description: 'Consumer Neuroscience Reactor via React, Gatsby, Prismic',
-    links: [{
-        label: 'WEBSITE',
-        url: 'https://cloud.army',
-    }],
-}
+    title: "CloudArmy",
+    duration: "July 2021 - Oct 2023",
+    jobTitle: "Software Developer",
+    description: "Consumer Neuroscience Reactor via React, Gatsby, Prismic",
+    links: [
+        {
+            label: "WEBSITE",
+            url: "https://cloud.army",
+        },
+    ],
+};
 const valostatsInfo = {
     image: valostatsLogo,
-    title: 'Valostats',
-    duration: '2020 - Front End Project',
-    jobTitle: '',
-    description: "A purely front end prototype React Website for Riot's FPS title, Valorant",
-    links: [{
-        label: 'DEMO',
-        url: 'https://bobaec.github.io/valostats/#/'
-    }, {
-        label: 'GITHUB',
-        url: 'https://github.com/bobaec/valostats',
-    }],
-}
+    title: "Valostats",
+    duration: "2020 - Front End Project",
+    jobTitle: "",
+    description:
+        "A purely front end prototype React Website for Riot's FPS title, Valorant",
+    links: [
+        {
+            label: "DEMO",
+            url: "https://bobaec.github.io/valostats/#/",
+        },
+        {
+            label: "GITHUB",
+            url: "https://github.com/bobaec/valostats",
+        },
+    ],
+};
 const buzzItInfo = {
     image: buzzItLogo,
-    title: 'BuzzIt Inc.',
-    duration: 'Oct 2020 - May 2021',
-    jobTitle: 'Front End Developer',
-    description: 'Worked on mainly web development; created notable features like chat and forum',
-    links: [{
-        label: 'WEBSITE',
-        url: 'https://buzzit.ca',
-    }],
-}
+    title: "BuzzIt Inc.",
+    duration: "Oct 2020 - May 2021",
+    jobTitle: "Front End Developer",
+    description:
+        "Worked on mainly web development; created notable features like chat and forum",
+    links: [
+        {
+            label: "WEBSITE",
+            url: "https://buzzit.ca",
+        },
+    ],
+};
 
 const sfuInfo = {
     image: sfuLogo,
-    title: 'Simon Fraser University',
-    duration: 'Apr 2020',
-    jobTitle: 'Software Systems',
-    description: 'Bachelor of Computing Science',
-    links: [{
-        label: 'WEBSITE',
-        url: 'https://sfu.ca',
-    }],
-}
+    title: "Simon Fraser University",
+    duration: "Apr 2020",
+    jobTitle: "Software Systems",
+    description: "Bachelor of Computing Science",
+    links: [
+        {
+            label: "WEBSITE",
+            url: "https://sfu.ca",
+        },
+    ],
+};
 
 const glanceInfo = {
     image: glanceLogo,
-    title: 'Glance',
-    duration: '2024 - Project in Development',
-    jobTitle: '',
-    description: 'React Native Dating App hosted on Firebase',
+    title: "Glance",
+    duration: "2024 - Project in Development",
+    jobTitle: "",
+    description: "React Native Dating App hosted on Firebase",
     // links: [{
     //     label: 'WEBSITE',
     //     url: 'https://sfu.ca',
     // }],
-}
+};
 
 const stonksInfo = {
     image: stonksLogo,
-    title: 'Stonks',
-    duration: 'Apr 2020 - Full Stack Project',
-    jobTitle: '',
-    description: "Implemented CoinMarketCap and Yahoo's finance API to retrieve stock and crypto info for analysis",
-    links: [{
-        label: 'GITHUB',
-        url: 'https://github.com/bobaec/Stocks',
-    }],
-}
+    title: "Stonks",
+    duration: "Apr 2020 - Full Stack Project",
+    jobTitle: "",
+    description:
+        "Implemented CoinMarketCap and Yahoo's finance API to retrieve stock and crypto info for analysis",
+    links: [
+        {
+            label: "GITHUB",
+            url: "https://github.com/bobaec/Stocks",
+        },
+    ],
+};
 
 const sfuSSEPInfo = {
     image: sfuLogo,
-    title: 'SFU Security',
-    duration: 'Sep 2017 - Dec 2017',
-    jobTitle: 'Full Stack Developer',
-    description: "Worked with SFU Security and Simon Fraser Student Society to create a Spring web application with payroll to manage volunteers and employees at SFU's Student Safety Engagement Program",
-    links: [{
-        label: 'WEBSITE',
-        url: 'https://www.sfu.ca/srs/campus-safety-security.html',
-    }],
-}
+    title: "SFU Security",
+    duration: "Sep 2017 - Dec 2017",
+    jobTitle: "Full Stack Developer",
+    description:
+        "Worked with SFU Security and Simon Fraser Student Society to create a Spring web application with payroll to manage volunteers and employees at SFU's Student Safety Engagement Program",
+    links: [
+        {
+            label: "WEBSITE",
+            url: "https://www.sfu.ca/srs/campus-safety-security.html",
+        },
+    ],
+};
 
 const mutationAnalysisInfo = {
     image: mutationAnalysisLogo,
-    title: 'Mutation Analysis',
-    duration: '2019 - Quality Assurance Project',
-    jobTitle: '',
-    description: "Analysis of multiple open source projects with mutation testing via Java's Major",
-    links: [{
-        label: 'GITHUB',
-        url: 'https://github.com/bobaec/Mutation-Analysis',
-    }],
-}
+    title: "Mutation Analysis",
+    duration: "2019 - Quality Assurance Project",
+    jobTitle: "",
+    description:
+        "Analysis of multiple open source projects with mutation testing via Java's Major",
+    links: [
+        {
+            label: "GITHUB",
+            url: "https://github.com/bobaec/Mutation-Analysis",
+        },
+    ],
+};
 
 const studywitmeInfo = {
     image: studywitmeLogo,
-    title: 'Studywitme',
-    duration: '2016 - Website Project',
-    jobTitle: '',
-    description: "Real time collaborative study platform for students at SFU hosted on Firebase",
-    links: [{
-        label: 'GITHUB',
-        url: 'https://github.com/bobaec/STUDYWITHME',
-    }],
-}
+    title: "Studywitme",
+    duration: "2016 - Website Project",
+    jobTitle: "",
+    description:
+        "Real time collaborative study platform for students at SFU hosted on Firebase",
+    links: [
+        {
+            label: "GITHUB",
+            url: "https://github.com/bobaec/STUDYWITHME",
+        },
+    ],
+};
 
-export { personalDescription, cloudArmyInfo, valostatsInfo, buzzItInfo, sfuInfo, glanceInfo, stonksInfo, sfuSSEPInfo, mutationAnalysisInfo, studywitmeInfo };
+const portfolioInfo = {
+    image: portfolioLogo,
+    title: "Portfolio",
+    duration: "2023",
+    jobTitle: "",
+    description: "Simple portfolio website made with React, SASS, and Firebase",
+    links: [
+        {
+            label: "GITHUB",
+            url: "https://github.com/bobaec/portfolio",
+        },
+    ],
+};
+
+export {
+    personalDescription,
+    cloudArmyInfo,
+    valostatsInfo,
+    buzzItInfo,
+    sfuInfo,
+    glanceInfo,
+    stonksInfo,
+    sfuSSEPInfo,
+    mutationAnalysisInfo,
+    studywitmeInfo,
+    portfolioInfo,
+};
